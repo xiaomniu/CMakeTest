2442b496f5e7d9618ccbdde25a53c7013da05e8c
xiaomniu <52282519+xiaomniu@users.noreply.github.com>
2024-10-28 17:32:01 +0800

    111

--- cpplib01/lib01.cpp
+++ cpplib01/lib01.cpp
@@ -1,6 +1,10 @@
 #include "lib01.h"
 #include "lib0101.h"
 
+
+double Add02(double a, double b) {
+    return a + b + Add01(a, b) + Add01(a, b);
+}
 double Add(double a, double b) {
-    return a + b + Add01(a, b);
+    return a + b + Add02(a, b);
 }
